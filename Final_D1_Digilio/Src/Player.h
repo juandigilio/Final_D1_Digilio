7ace69fc9b0509de5d544569b94104b4f1332966
juandigilio <juanignaciodigilio@gmail.com>
2024-02-27 00:17:54 -0300

    MainScreen images modified
    all jpg files updated to png

--- Final_D1_Digilio/Src/Player.h
+++ Final_D1_Digilio/Src/Player.h
@@ -7,21 +7,20 @@
 
 using namespace GameData;
 
-static const int maxBulletsQnty = 30;
 
 struct Player
 {
     Vector2 position{};
     Vector2 velocity{};
-    float rotation = 0.0f;
-    float maxSpeed{};
+    //float rotation = 0.0f;
+    float maxSpeed = 150.0f;
 
     bool isAlive = true;
 
     Texture2D texture{};
     Rectangle source{};
     int frame{};
-    double lastFrame{};
+    double lastFrame = 0.0f;
     bool isWalking = false;
     float lastCollide = 0.0f;
 
@@ -34,11 +33,11 @@ struct Player
 
 namespace PlayerUtilities
 {
-    extern void LoadPlayer(Player& player1, Player& player2, Player& player3);
+    extern void LoadPlayer(Player& player);
 
-    extern void GetPlayerInput(Player& player, GameSceen& currentSceen);
+    extern void GetPlayerInput(Player& player, GameScreen& currentScreen);
 
-    extern void UpdatePlayer(Player& player, Rectangle level);
+    extern void UpdatePlayer(Player& player, GameScreen& currentScreen);
 
     extern void DrawPlayer(Player& player);
 
