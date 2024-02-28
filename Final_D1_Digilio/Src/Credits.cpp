1111c57773891c1eb3dc1965c0a76d6e101f3921
juandigilio <juanignaciodigilio@gmail.com>
2024-02-28 00:33:57 -0300

    Credits finished

--- Final_D1_Digilio/Src/Credits.cpp
+++ Final_D1_Digilio/Src/Credits.cpp
@@ -10,19 +10,20 @@ namespace Credits
 	{
 		Vector2 actualPos = playPosCenter;
 
-		actualPos.x = actualPos.x + gameScreen.width * 2.0f - MeasureTextEx(font, "Created by Juan Digilio", fontSize * 0.65f, spacing / 4.0f).x / 2.0f;
-		actualPos.y -= 60;
-		DrawTextEx(font, "Created by Juan Digilio", actualPos, fontSize * 0.65f, spacing / 4.0f, BLACK);
+		actualPos.x = actualPos.x + gameScreen.width * 2.0f - (MeasureTextEx(font, "Created by Juan Digilio", fontSize * 0.65f, spacing / 4.0f).x / 2.0f);
+		actualPos.y -= 30;
+		DrawTextEx(font, "Created by Juan Digilio", actualPos, fontSize * 0.65f, spacing / 4.0f, WHITE);
+
+		actualPos = playPosCenter;
+		actualPos.y += 10;
+
+		actualPos.x = actualPos.x + gameScreen.width * 2.0f - (MeasureTextEx(font, "Game art by Juan Digilio", fontSize * 0.4f, spacing / 8.0f).x / 2.0f);
+		DrawTextEx(font, "Game art by Juan Digilio", actualPos, fontSize * 0.4f, spacing / 8.0f, WHITE);
 
 		actualPos = playPosCenter;
 
-		actualPos.x = actualPos.x + gameScreen.width * 2.0f - MeasureTextEx(font, "Game art by Juan Digilio", fontSize * 0.4f, spacing / 8.0f).x / 2.0f;
-		DrawTextEx(font, "Game art by Juan Digilio", actualPos, fontSize * 0.4f, spacing / 8.0f, BLACK);
-
-		actualPos = playPosCenter;
-
-		actualPos.x = actualPos.x + gameScreen.width * 2.0f - MeasureTextEx(font, "Cars assets by TheMemeLord4", fontSize * 0.25f, spacing / 8.0f).x / 2.0f;
+		actualPos.x = actualPos.x + gameScreen.width * 2.0f - (MeasureTextEx(font, "Cars assets by TheMemeLord4", fontSize * 0.25f, spacing / 8.0f).x / 2.0f);
 		actualPos.y += 60;
-		DrawTextEx(font, "Cars assets by TheMemeLord4", actualPos, fontSize * 0.25f, spacing / 8.0f, BLACK);
+		DrawTextEx(font, "Cars assets by TheMemeLord4", actualPos, fontSize * 0.25f, spacing / 8.0f, WHITE);
 	}
 }
