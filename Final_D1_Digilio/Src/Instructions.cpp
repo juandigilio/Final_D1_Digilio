72d14fe38b3a06f5bd8f71bfded7be487d21d8ae
juandigilio <juanignaciodigilio@gmail.com>
2024-03-01 11:50:08 -0300

    Instructions info updated

--- Final_D1_Digilio/Src/Instructions.cpp
+++ Final_D1_Digilio/Src/Instructions.cpp
@@ -10,10 +10,15 @@ namespace Instructions
 		Vector2 actualPos = playPosCenter;
 
 		actualPos.x = actualPos.x + gameScreen.width - (MeasureTextEx(font, "INSTRUCTIONS", fontSize * 0.85f, spacing / 2.0f).x / 2.0f);
+		actualPos.y -= 75.0f;
 		DrawTextEx(font, "INSTRUCTIONS", actualPos, fontSize * 0.85f, spacing / 2.0f, RAYWHITE);
 
+		actualPos.x = playPosCenter.x + +gameScreen.width - (MeasureTextEx(font, "Use arrows to cut lines, press ENTER to go back to car selection", instructionsSize, spacing / 4.0f).x / 2.0f);
+		actualPos.y += fontSize * 2.0f;
+		DrawTextEx(font, "Use arrows to cut lines, press ENTER to go back to car selection", actualPos, instructionsSize, spacing / 4.0f, RAYWHITE);
+
 		actualPos.x = playPosCenter.x + +gameScreen.width - (MeasureTextEx(font, "Don't crash and earn 1.000 points to win the game", instructionsSize, spacing / 4.0f).x / 2.0f);
-		actualPos.y += fontSize;
+		actualPos.y += fontSize / 2.0f;
 		DrawTextEx(font, "Don't crash and earn 1.000 points to win the game", actualPos, instructionsSize, spacing / 4.0f, RAYWHITE);
 	}
 }
